535318fbc6ac3c4ebf39617e5e6d41b89f6df5cb
ericgu <eric_gunnerson@hotmail.com>
2014-11-21 13:39:30 -0800

    Extracted recipe operations into static methods

--- Form1.cs
+++ Form1.cs
@@ -13,7 +13,7 @@ namespace RecipeManager
 {
     public partial class Form1 : Form
     {
-        private List<Recipe> m_recipes = new List<Recipe>(); 
+        private List<Recipe> m_recipes = new List<Recipe>();
 
         public Form1()
         {
@@ -25,7 +25,8 @@ namespace RecipeManager
 
         private string GetRecipeDirectory()
         {
-            string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RecipeMaker");
+            string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "RecipeMaker");
 
             if (File.Exists(directory + @"\" + "RecipeDirectory.txt"))
             {
@@ -41,22 +42,36 @@ namespace RecipeManager
 
         private void SetRecipeDirectory(string recipeDirectory)
         {
-            string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RecipeMaker");
+            string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "RecipeMaker");
 
             File.WriteAllText(directory + @"\" + "RecipeDirectory.txt", recipeDirectory);
         }
 
-        private void LoadRecipes()
-        {
-            string directory = GetRecipeDirectory();
-            DirectoryInfo directoryInfo = new DirectoryInfo(directory);
-            directoryInfo.Create();
+private void LoadRecipes()
+{
+    string directory = GetRecipeDirectory();
+    m_recipes = LoadRecipesPort(directory);
 
-            m_recipes = directoryInfo.GetFiles("*")
-                .Select(fileInfo => new Recipe { Name = fileInfo.Name, Size = fileInfo.Length, Text = File.ReadAllText(fileInfo.FullName) }).ToList();
+    PopulateList();
+}
 
-            PopulateList();
-        }
+private static List<Recipe> LoadRecipesPort(string directory)
+{
+    DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+    directoryInfo.Create();
+
+    return directoryInfo.GetFiles("*")
+        .Select(
+            fileInfo =>
+                new Recipe
+                {
+                    Name = fileInfo.Name,
+                    Size = fileInfo.Length,
+                    Text = File.ReadAllText(fileInfo.FullName)
+                })
+        .ToList();
+}
 
         private void PopulateList()
         {
@@ -75,12 +90,17 @@ namespace RecipeManager
                 m_recipes.Remove(recipeListViewItem.Recipe);
                 string directory = GetRecipeDirectory();
 
-                File.Delete(directory + @"\" + recipeListViewItem.Recipe.Name);
+                DeleteRecipe(directory, recipeListViewItem.Recipe.Name);
             }
             PopulateList();
 
             NewClick(null, null);
-        } 
+        }
+
+        private static void DeleteRecipe(string directory, string name)
+        {
+            File.Delete(directory + @"\" + name);
+        }
 
         private void NewClick(object sender, EventArgs e)
         {
@@ -92,9 +112,14 @@ namespace RecipeManager
         {
             string directory = GetRecipeDirectory();
 
-            File.WriteAllText(Path.Combine(directory, textBoxName.Text), textBoxObjectData.Text);
+            SaveRecipe(directory, textBoxName.Text, textBoxObjectData.Text);
             LoadRecipes();
         }
+
+private static void SaveRecipe(string directory, string name, string directions)
+{
+    File.WriteAllText(Path.Combine(directory, name), directions);
+}
 
         private void SelectedIndexChanged(object sender, EventArgs e)
         {
